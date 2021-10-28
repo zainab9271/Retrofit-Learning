343db70b3a4a140173b66095d95e60d340203238
Zainab Mahmood <zainab9271mahmood@gmail.com>
2021-10-28 11:24:21 +0300

    update the repository to use kotlin flow to return a random joke

--- app/src/main/java/com/zainab/retrofitlearning/JokeRepository.kt
+++ app/src/main/java/com/zainab/retrofitlearning/JokeRepository.kt
@@ -1,7 +1,14 @@
 package com.zainab.retrofitlearning
 
+import com.zainab.retrofitlearning.model.JokeResponse
 import com.zainab.retrofitlearning.networking.API
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class JokeRepository {
-    suspend fun getRandomJoke() = API.apiService.getRandomJoke()
+    suspend fun getRandomJoke(): Flow<JokeResponse?>{
+        return flow {
+            emit(API.apiService.getRandomJoke().body())
+        }
+    }
 }
