bf38c529dce0ea88ff26620802d1ea66f572b72c
Zainab Mahmood <zainab9271mahmood@gmail.com>
2021-10-28 11:14:41 +0300

    make the request function returns Response<JokeResponse>

--- app/src/main/java/com/zainab/retrofitlearning/networking/JokeApiService.kt
+++ app/src/main/java/com/zainab/retrofitlearning/networking/JokeApiService.kt
@@ -1,11 +1,12 @@
 package com.zainab.retrofitlearning.networking
 
 import com.zainab.retrofitlearning.model.JokeResponse
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface JokeApiService{
 
     @GET("joke/Any")
-    suspend fun getRandomJoke(): JokeResponse
+    suspend fun getRandomJoke(): Response<JokeResponse>
 
 }
